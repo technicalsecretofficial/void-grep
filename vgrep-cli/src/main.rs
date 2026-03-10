76df1f5d318e243da50811bd61e30c624bdeebf9
technicalsecretofficial <technicalsecretofficial@gmail.com>
2026-03-10 22:14:26 +0500

    positional argument and bufreader for file reading is added successfully phase -1

--- vgrep-cli/src/main.rs
+++ vgrep-cli/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+
+struct Args {
+  
+    value: String,
+
+    file: String,
+    
+}
+
+fn main() -> io::Result<()>   {
+    let args = Args::parse();
+
+
+    let file = File::open(args.file)?;
+
+    let reader = BufReader::new(file);
+
+    let mut word = String::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        
+        word = line.clone();
+        
+        if word.contains(&args.value) {
+            println!("Found: {}", args.value);
+            break;
+        }
+            
+     }
+
+        if !word.contains(&args.value)  {
+            println!("Not Found");
+        }
+
+
+
+    Ok(())
+ 
 }
